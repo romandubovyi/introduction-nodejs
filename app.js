21201c77a1270015f0a697f1bd51db5f64cda703
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 15:42:10 +0300

    First basic express code

--- app.js
+++ app.js
@@ -1,22 +1,14 @@
-const http = require('http');
+const express = require('express');
 
-const server = http.createServer((request, response) => {
-    let body = [];
-    request.on('data', chunk => {
-        body.push(chunk);
-    });
-    request.on('end', () => {
-        body = Buffer.concat(body).toString();
-        let userName = 'Unknown User';
-        if (body) {
-            userName = body.split('=')[1];
-        }
-        response.setHeader('Content-Type', 'text/html');
-        response.write(
-            `<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`
-        );
-        response.end();
-    });
+const app = express();
+
+app.use((req, res, next) => {
+    res.setHeader('Content-Type', 'text/html');
+    next();
 });
 
-server.listen(3000);
+app.use((req, res, next) => {
+    res.send('<h1>Hello World!</h1>');
+});
+
+app.listen(3000);
