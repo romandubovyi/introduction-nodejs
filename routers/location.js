ec14073b56e7b8e81ac2e9fcef76df02eaf423cf
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 15:55:59 +0300

    Send id to frontend

--- routers/location.js
+++ routers/location.js
@@ -7,12 +7,13 @@ const locationStorage = {
 };
 
 router.post('/add-post', (req, res, next) => {
+    const id = Math.random();
     locationStorage.locations.push({
         id: Math.random(),
         title: req.body.title,
         description: req.body.description
     });
-    res.json({message: 'Stored post!'});
+    res.json({message: 'Stored post!', postId: id});
 });
 
 router.get('/posts', (req, res, next) => {});
