cecb2fdfa182b8b624907d29cbb1ffbc0bd383e0
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 15:34:57 +0300

    Added first server

--- app.js
+++ app.js
@@ -1,17 +1,8 @@
-const fs = require('fs');
+const http = require('http');
 
-fs.readFile('user-data.txt', (err, data) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(data.toString());
+const server = http.createServer((request, response) => {
+    response.write('hello there!');
+    response.end();
 });
 
-fs.writeFile('user-data.txt', 'username=Max', err => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log('Wrote to file!');
-    }
-});
+server.listen(3000);
