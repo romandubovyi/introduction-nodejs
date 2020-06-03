200643f857f34370a3c2ca4f5d10eb1c9a084391
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 16:00:18 +0300

    Get post route

--- routers/location.js
+++ routers/location.js
@@ -2,13 +2,13 @@ const express = require('express');
 
 const router = express.Router();
 
-const locationStorage = {
-    locations: []
+const postStorage = {
+    posts: []
 };
 
 router.post('/add-post', (req, res, next) => {
     const id = Math.random();
-    locationStorage.locations.push({
+    postStorage.posts.push({
         id: Math.random(),
         title: req.body.title,
         description: req.body.description
@@ -16,6 +16,15 @@ router.post('/add-post', (req, res, next) => {
     res.json({message: 'Stored post!', postId: id});
 });
 
-router.get('/posts', (req, res, next) => {});
+router.get('/post/:pid', (req, res, next) => {
+    const postId = +req.params.lid;
+    const post = locationStorage.posts.find(post => {
+        return post.id === postId;
+    });
+    if (!post) {
+        return res.status(404).json({ message: 'Not found!' });
+    }
+    res.json({ title: post.title, description: post.description });
+});
 
 module.exports = router;
