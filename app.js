7df53a0c737fb92040f23f4398ffc510381c9b16
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 15:48:33 +0300

    Dinamic html templates ejs

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
@@ -12,7 +15,9 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
     const userName = req.body.username || 'Unknown User';
-    res.send(`<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`);
+    res.render('index', {
+        user: userName
+    });
 });
 
 app.listen(3000);
