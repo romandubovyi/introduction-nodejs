9016d50093e1ddec7c7ba6e0b02a16166f5c0f0e
Roman Dubovyi <r.dubovoi@gmail.com>
2020-06-03 15:45:28 +0300

    Parsing request body

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.use((req, res, next) => {
     res.setHeader('Content-Type', 'text/html');
@@ -8,7 +11,8 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    res.send('<h1>Hello World!</h1>');
+    const userName = req.body.username || 'Unknown User';
+    res.send(`<h1>Hi ${userName}</h1><form method="POST" action="/"><input name="username" type="text"><button type="submit">Send</button></form>`);
 });
 
 app.listen(3000);
